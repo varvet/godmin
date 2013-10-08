bd35fa098a68d73352c1221e932593505343175d
Jens Ljungblad <jens.ljungblad@gmail.com>
2013-10-08 08:56:54 +0200

    Adds overriding of templates magic

--- app/helpers/godmin/application_helper.rb
+++ app/helpers/godmin/application_helper.rb
@@ -1,4 +1,15 @@
 module Godmin
   module ApplicationHelper
+
+    def render_partial(template, locals: {})
+      template_name = "admin/#{template}"
+
+      unless lookup_context.exists?(template_name, nil, true)
+        template_name = "godmin/#{template}"
+      end
+
+      render partial: template_name, locals: locals
+    end
+
   end
 end
