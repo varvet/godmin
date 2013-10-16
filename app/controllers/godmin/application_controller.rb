ed31edde24a8c491d4980ae2755661ec17c08f47
Jens Ljungblad <jens.ljungblad@gmail.com>
2013-10-16 09:32:58 +0200

    Adds authenticatability

--- app/controllers/godmin/application_controller.rb
+++ app/controllers/godmin/application_controller.rb
@@ -1,11 +1,18 @@
 module Godmin
   class ApplicationController < ActionController::Base
+    before_filter :authenticate_user
     before_filter :prepend_view_paths
 
     private
 
+    def authenticate_user
+      if Godmin.authentication_method
+        self.send(Godmin.authentication_method)
+      end
+    end
+
     def prepend_view_paths
-     prepend_view_path "app/views/admin"
+      prepend_view_path "app/views/admin"
     end
 
   end
--- lib/godmin.rb
+++ lib/godmin.rb
@@ -1,9 +1,20 @@
 require 'inherited_resources'
 require 'kaminari'
 require 'godmin/engine'
-require 'godmin/version'
 require 'godmin/rails'
+require 'godmin/version'
 
 module Godmin
   mattr_accessor :mounted_as
+  self.mounted_as = nil
+
+  mattr_accessor :authentication_method
+  self.authentication_method = false
+
+  mattr_accessor :current_user_method
+  self.current_user_method = false
+
+  def self.configure
+    yield self
+  end
 end
