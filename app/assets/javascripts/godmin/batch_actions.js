91f9018403a3973345da6f3b057bdc57b5c889fe
Jens Ljungblad <jens.ljungblad@gmail.com>
2013-10-23 11:54:04 +0200

    Changes the batch process gui

--- app/assets/javascripts/godmin/batch_actions.js
+++ app/assets/javascripts/godmin/batch_actions.js
@@ -1,9 +1,20 @@
 (function($){
   $(function(){
+
+    $form = $('#batch-process-form');
+
+    $form.find('input:checkbox').on('click', function(){
+      if ($form.find('input:checkbox:checked').length) {
+        $('.batch-process-action-link').removeClass('disabled');
+      } else {
+        $('.batch-process-action-link').addClass('disabled');
+      }
+    });
+
     $(document).delegate('.batch-process-action-link', 'click.rails', function(){
-      $form = $('#batch-process-form');
       $form.find('#batch-process-action').val($(this).data('value'));
       $form.submit();
     });
+
   });
 }(jQuery));
