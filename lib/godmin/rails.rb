84c89be47380a827f3033c3690ed57ce0f614f21
Jens Ljungblad <jens.ljungblad@gmail.com>
2014-03-19 15:47:37 +0100

    Revert changes

--- lib/godmin/rails.rb
+++ lib/godmin/rails.rb
@@ -1,7 +1,7 @@
 module ActionDispatch::Routing
   class Mapper
 
-    def godmin_for(name)
+    def embrace_godmin(name)
       mount Godmin::Engine, at: name
 
       namespace name.to_sym do
@@ -11,15 +11,32 @@ module ActionDispatch::Routing
       Godmin.mounted_as = name
     end
 
-    def resources(*resources, &block)
-      super do
+    def watches_over(resource)
+      resources resource do
         post "batch_action", on: :collection
+        yield if block_given?
       end
     end
 
-    # TODO: legacy names
-    alias_method :embrace_godmin, :godmin_for
-    alias_method :watches_over, :resources
+    # def godmin_for(name)
+    #   mount Godmin::Engine, at: name
+
+    #   namespace name.to_sym do
+    #     yield
+    #   end
+
+    #   Godmin.mounted_as = name
+    # end
+
+    # def resources(*resources, &block)
+    #   super do
+    #     post "batch_action", on: :collection
+    #   end
+    # end
+
+    # # TODO: legacy names
+    # alias_method :embrace_godmin, :godmin_for
+    # alias_method :watches_over, :resources
 
   end
 end
