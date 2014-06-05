cfec9f5f84d18fda1b3aa8816454e4e5be30c733
Linus Pettersson <linus.pettersson@gmail.com>
2014-06-05 14:52:32 +0200

    Renamed translation helper method and added column action translations

--- app/helpers/godmin/translate_helper.rb
+++ app/helpers/godmin/translate_helper.rb
@@ -1,6 +1,6 @@
 module Godmin
   module TranslateHelper
-    def godmin_translate_for_resource(translate, default: nil)
+    def godmin_translate(translate, default: nil)
       I18n.t("godmin.#{resource_class.to_s.underscore}.#{translate}", resource: resource_class, default: default || I18n.t("godmin.#{translate}", resource: resource_class))
     end
   end
