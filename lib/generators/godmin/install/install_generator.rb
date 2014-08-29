273b5a38a8ba631d012c215a466baff55bbed32f
Jens Ljungblad <jens.ljungblad@gmail.com>
2014-08-29 10:11:11 +0200

    Install generator up and running

--- lib/generators/godmin/install/install_generator.rb
+++ lib/generators/godmin/install/install_generator.rb
@@ -1,5 +1,28 @@
 class Godmin::InstallGenerator < Rails::Generators::NamedBase
 
-  # TODO: implement this class :)
+  def create_initializer
+    create_file "#{@name}/config/initializers/godmin.rb" do
+      <<-END.strip_heredoc
+        Godmin.configure do |config|
+          config.mounted_as = "#{@name}"
+        end
+      END
+    end
+  end
+
+  def create_routes
+    inject_into_file "#{@name}/config/routes.rb", before: /^end/ do
+      <<-END.strip_heredoc.indent(2)
+        godmin do
+        end
+      END
+    end
+  end
+
+  def create_controller
+    gsub_file "#{@name}/app/controllers/#{@name}/application_controller.rb", "ActionController::Base" do |match|
+      "Godmin::ApplicationController"
+    end
+  end
 
 end
