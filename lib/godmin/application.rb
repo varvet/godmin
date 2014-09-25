f8db048ca67894df5bfc117cc5ad5b2ba88522fd
Jens Ljungblad <jens.ljungblad@gmail.com>
2014-09-25 17:18:41 +0200

    Experimenting with a resolver

--- lib/godmin/application.rb
+++ lib/godmin/application.rb
@@ -3,40 +3,26 @@ module Godmin
     extend ActiveSupport::Concern
 
     included do
-      include Godmin::Helpers::Render
+      # include Godmin::Helpers::Render
       include Godmin::Helpers::Translate
 
+      append_view_path Godmin::Resolver.new(:app)
+      append_view_path Godmin::Resolver.new(:godmin)
 
-      # append_view_path Godmin::Resolver.new
-
-      # before_action :set_resolver_foo_yeah
-      # before_action :set_lookup_context
-
-      # layout "godmin/application"
-    end
-
-    # TODO: works but without layout
-    def _prefixes
-      ["godmin/application"]
+      layout "godmin/application"
     end
 
     def welcome
-      # render "welcome-no"
-      # godmin_render_template "welcome"
-      # render "godmin/application/welcome", layout: "godmin/application"
     end
 
     private
 
-    # def set_resolver_foo_yeah
-    #   debugger
-    #   puts "yeah"
-    #   # append_view_path Godmin::Resolver.new("app/views")
-    # end
-
-    # TODO: works but without layout
-    # def set_lookup_context
-    #   view_context.lookup_context.prefixes << "godmin/application"
+    # def _prefixes
+    #   super + [
+    #     [Godmin.mounted_as, "resource"].compact.join("/"),
+    #     "godmin/#{controller_path.split("/").last}",
+    #     "godmin/resource"
+    #   ]
     # end
   end
 end
--- lib/godmin/resolver.rb
+++ lib/godmin/resolver.rb
@@ -1,23 +1,39 @@
 module Godmin
   class Resolver < ::ActionView::FileSystemResolver
-    def initialize
-      super("app/views")
-      debugger
-      puts "hello"
+
+    def initialize(type)
+      case type
+      when :app
+        super File.join(Godmin.mounted_as, "app/views")
+      when :godmin
+        super File.join(Godmin::Engine.root, "app/views")
+      end
+      @type = type
     end
 
     def find_templates(name, prefix, partial, details)
-    #   # prefix = ["godmin", prefix.split("/").last].join("/")
-    #   #
-    #   # x = super(name, prefix, partial, details)
-    #   # debugger
-    #   #
-    #   # x
-    #
-    #   super("welcome-no", prefix, partial, details)
-    #
-      debugger
-      super
+      paths = []
+
+      case @type
+      when :app
+        paths << [Godmin.mounted_as, prefix.split("/").last].compact.join("/")
+        paths << [Godmin.mounted_as, "resource"].compact.join("/")
+        paths << [Godmin.mounted_as].compact.join("/")
+      when :godmin
+        paths << "godmin/#{prefix.split("/").last}"
+        paths << "godmin/resource"
+        paths << "godmin"
+      end
+
+      template = []
+
+      paths.each do |path|
+        template = super(name, path, partial, details)
+        break if template.present?
+      end
+
+      template
     end
+
   end
 end
