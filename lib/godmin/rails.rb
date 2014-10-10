67cc4c4096dfd6466950b949b4e803db7a35fdab
Jens Ljungblad <jens.ljungblad@gmail.com>
2014-10-10 16:21:19 +0200

    Removes devise stuff from route overrides

--- lib/godmin/rails.rb
+++ lib/godmin/rails.rb
@@ -2,11 +2,11 @@ module ActionDispatch::Routing
   class Mapper
 
     def godmin
-      override_devise_for do
+      # override_devise_for do
         override_resources do
           yield
         end
-      end
+      # end
 
       unless has_named_route?(:root)
         root to: "application#welcome"
@@ -15,27 +15,27 @@ module ActionDispatch::Routing
 
     private
 
-    # TODO: it would be nice if this could be replaced with super calls
-    alias_method :_devise_for, :devise_for
-
-    def override_devise_for
-      def devise_for(*resources)
-        _devise_for(*resources, {
-          router_name: Godmin.mounted_as,
-          controllers: {
-            passwords: "godmin/devise/passwords",
-            registrations: "godmin/devise/registrations",
-            sessions: "godmin/devise/sessions"
-          }
-        }.deep_merge(resources.extract_options!))
-      end
-
-      yield
-
-      def devise_for(*resources)
-        _devise_for(*resources)
-      end
-    end
+    # # TODO: it would be nice if this could be replaced with super calls
+    # alias_method :_devise_for, :devise_for
+    #
+    # def override_devise_for
+    #   def devise_for(*resources)
+    #     _devise_for(*resources, {
+    #       router_name: Godmin.mounted_as,
+    #       controllers: {
+    #         passwords: "godmin/devise/passwords",
+    #         registrations: "godmin/devise/registrations",
+    #         sessions: "godmin/devise/sessions"
+    #       }
+    #     }.deep_merge(resources.extract_options!))
+    #   end
+    #
+    #   yield
+    #
+    #   def devise_for(*resources)
+    #     _devise_for(*resources)
+    #   end
+    # end
 
     # TODO: it would be nice if this could be replaced with super calls
     alias_method :_resources, :resources
