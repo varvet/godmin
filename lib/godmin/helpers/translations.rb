d681ec32709a5423c34fee07de5ce0b452cd3380
Jens Ljungblad <jens.ljungblad@gmail.com>
2014-10-10 14:11:40 +0200

    Fix hound complaint

--- lib/godmin/helpers/translations.rb
+++ lib/godmin/helpers/translations.rb
@@ -9,7 +9,11 @@ module Godmin
 
       def godmin_translate(translate, default: nil)
         if @resource_class
-          view_context.t("godmin.#{@resource_class.to_s.underscore}.#{translate}", resource: @resource_class.model_name.human, default: default || view_context.t("godmin.#{translate}", resource: @resource_class.model_name.human))
+          view_context.t(
+            "godmin.#{@resource_class.to_s.underscore}.#{translate}",
+            resource: @resource_class.model_name.human,
+            default: default || view_context.t("godmin.#{translate}", resource: @resource_class.model_name.human)
+          )
         else
           view_context.t("godmin.#{translate}")
         end
