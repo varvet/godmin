4b75791c82ca4b37153c816a56e18b1664ec506f
Jens Ljungblad <jens.ljungblad@gmail.com>
2014-10-21 16:42:15 +0200

    Moves sessions routes to godmin

--- lib/godmin/rails.rb
+++ lib/godmin/rails.rb
@@ -1,12 +1,14 @@
 module ActionDispatch::Routing
   class Mapper
-
     def godmin
-      # override_devise_for do
       override_resources do
         yield
       end
-      # end
+
+      # TODO: something goes wrong with view context, helpers etc
+      if Godmin.admin_user_class
+        resources :sessions, only: [:new, :create, :destroy], controller: "/godmin/sessions"
+      end
 
       unless has_named_route?(:root)
         root to: "application#welcome"
@@ -15,38 +17,13 @@ module ActionDispatch::Routing
 
     private
 
-    # # TODO: it would be nice if this could be replaced with super calls
-    # alias_method :_devise_for, :devise_for
-    #
-    # def override_devise_for
-    #   def devise_for(*resources)
-    #     _devise_for(*resources, {
-    #       router_name: Godmin.mounted_as,
-    #       controllers: {
-    #         passwords: "godmin/devise/passwords",
-    #         registrations: "godmin/devise/registrations",
-    #         sessions: "godmin/devise/sessions"
-    #       }
-    #     }.deep_merge(resources.extract_options!))
-    #   end
-    #
-    #   yield
-    #
-    #   def devise_for(*resources)
-    #     _devise_for(*resources)
-    #   end
-    # end
-
-    # TODO: it would be nice if this could be replaced with super calls
-    alias_method :_resources, :resources
-
     def override_resources
       def resources(*resources)
         unless Godmin.resources.include?(resources.first)
           Godmin.resources << resources.first
         end
 
-        _resources(*resources) do
+        super(*resources) do
           yield if block_given?
           post "batch_action", on: :collection
         end
@@ -55,9 +32,8 @@ module ActionDispatch::Routing
       yield
 
       def resources(*resources, &block)
-        _resources(*resources, &block)
+        super(*resources, &block)
       end
     end
-
   end
 end
