89b3e50110fe8d565b77e17f66696f2b58521682
Jens Ljungblad <jens.ljungblad@gmail.com>
2014-10-22 10:08:45 +0200

    Added some todos

--- lib/godmin/application.rb
+++ lib/godmin/application.rb
@@ -19,6 +19,10 @@ module Godmin
       append_view_path Godmin::GodminResolver.new(controller_name)
     end
 
+    # TODO: think about how to extract this.. should there be an
+    # Godmin::Helpers::Authentication module for the helpers and
+    # a Godmin::Application::Authentication module for the
+    # authenticate_user callback? when helper/not helper...
     concerning :Authentication do
       included do
         before_action :authenticate_user
@@ -27,17 +31,19 @@ module Godmin
         helper_method :admin_user_signed_in?
       end
 
+      # TODO: implement correct routes
+      # TODO: think about moving conditionals somewhere else, signed in especially?
       def authenticate_user
-        if Godmin.admin_user_class
-          unless admin_user_signed_in? || controller_name == "sessions"
-            redirect_to "/admin/sessions/new", alert: "Authentication needed"
-          end
+        if Godmin.admin_user_class && !admin_user_signed_in? && controller_name != "sessions"
+          redirect_to "/admin/sessions/new", alert: "Authentication needed"
         end
       end
 
+      # TODO: memoize this value? what about when signing out?
+      # TODO: if called current_admin_user or something, does it conflict with devise?
       def admin_user
         if Godmin.admin_user_class && session[:admin_user_id]
-          Godmin.admin_user_class.find(session[:admin_user_id])
+          Godmin.admin_user_class.find_by(id: session[:admin_user_id])
         end
       end
 
