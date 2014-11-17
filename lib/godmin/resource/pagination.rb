161e1799a61de2ccc454b59edb3cfb6fc112b008
Jens Ljungblad <jens.ljungblad@gmail.com>
2014-11-17 22:18:58 +0100

    Implements simple pagination

--- lib/godmin/resource/pagination.rb
+++ lib/godmin/resource/pagination.rb
@@ -3,8 +3,32 @@ module Godmin
     module Pagination
       extend ActiveSupport::Concern
 
+      included do
+        helper_method :pages
+      end
+
       def apply_pagination(resources)
-        resources.page(params[:page])
+        resources.limit(pagination_limit).offset(pagination_offset)
+      end
+
+      private
+
+      def pagination_limit
+        self.class.per_page
+      end
+
+      def pagination_offset
+        self.class.per_page - (params[:page].to_i * self.class.per_page)
+      end
+
+      def pages
+        1..(resources.count.to_f / self.class.per_page).ceil
+      end
+
+      module ClassMethods
+        def per_page
+          10
+        end
       end
     end
   end
