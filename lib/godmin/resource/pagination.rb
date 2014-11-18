465d8c40edbca4d98f6f1f501f71292a8dba9a09
Jens Ljungblad <jens.ljungblad@gmail.com>
2014-11-18 16:14:28 +0100

    Pagination stuff

--- lib/godmin/resource/pagination.rb
+++ lib/godmin/resource/pagination.rb
@@ -4,6 +4,7 @@ module Godmin
       extend ActiveSupport::Concern
 
       included do
+        helper_method :current_page
         helper_method :pages
       end
 
@@ -13,16 +14,35 @@ module Godmin
 
       private
 
+      def current_page
+        params[:page].present? ? params[:page].to_i : 1
+      end
+
       def pagination_limit
         self.class.per_page
       end
 
       def pagination_offset
-        self.class.per_page - (params[:page].to_i * self.class.per_page)
+        (params[:page].to_i * self.class.per_page) - self.class.per_page
       end
 
+      # threshold: 5
+      # 1,2,(3),4,5,6,7,8,9,10,11,12 => 1,2,(3),4,5...
+      # 1,2,3,4,5,6,7,8,(9),10,11,12 => ...8,(9),10,11,12
+      # 1,2,3,4,5,6,(7),8,9,10,11,12 => ...5,6,(7),8,9...
+
       def pages
-        1..(resources.count.to_f / self.class.per_page).ceil
+        x = (1..(resource_class.count.to_f / self.class.per_page).ceil)
+
+        if x.count > 5
+          if current_page < 5
+            x.to_a.slice(0, 5)
+          elsif current_page > (x.count - 5)
+            x.to_a.slice(-5, 5)
+          end
+        else
+          x
+        end
       end
 
       module ClassMethods
