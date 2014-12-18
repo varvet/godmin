0e023c62fc10d5b5d456faaa5afda5f01ae09beb
Jens Ljungblad <jens.ljungblad@gmail.com>
2014-12-18 13:06:47 +0100

    Additional pickers

--- app/assets/javascripts/godmin/datetimepickers.js
+++ app/assets/javascripts/godmin/datetimepickers.js
@@ -9,15 +9,34 @@ Godmin.Datetimepickers = (function() {
   function initializeEvents() {}
 
   function initializeState() {
+    initializeDatepicker($('[data-behavior=datepicker]'));
+    initializeTimepicker($('[data-behavior=timepicker]'));
     initializeDatetimepicker($('[data-behavior=datetimepicker]'));
   }
 
-  function initializeDatetimepicker($el) {
-    $el.datetimepicker();
+  function initializeDatepicker($el, options) {
+    var defaults = {
+      pickTime: false
+    };
+    initializeDatetimepicker($el, $.extend(defaults, options));
+  }
+
+  function initializeTimepicker($el, options) {
+    var defaults = {
+      pickDate: false
+    };
+    initializeDatetimepicker($el, $.extend(defaults, options));
+  }
+
+  function initializeDatetimepicker($el, options) {
+    var defaults = {};
+    $el.datetimepicker($.extend(defaults, options));
   }
 
   return {
     initialize: initialize,
+    initializeDatepicker: initializeDatepicker,
+    initializeTimepicker: initializeTimepicker,
     initializeDatetimepicker: initializeDatetimepicker
   };
 })();
