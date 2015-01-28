7d5af8b71f2c8377ea23feb6b958f9961fb832dc
Jens Ljungblad <jens.ljungblad@gmail.com>
2015-01-28 15:23:00 +0100

    Renames controller generator back to resource generator

--- lib/generators/godmin/resource/resource_generator.rb
+++ lib/generators/godmin/resource/resource_generator.rb
@@ -1,4 +1,4 @@
-class Godmin::ControllerGenerator < Rails::Generators::NamedBase
+class Godmin::ResourceGenerator < Rails::Generators::NamedBase
   source_root File.expand_path("../templates", __FILE__)
   argument :attributes, type: :array, default: [], banner: "attribute attribute"
 
@@ -7,12 +7,12 @@ class Godmin::ControllerGenerator < Rails::Generators::NamedBase
   def add_route
     inject_into_file "config/routes.rb", after: "godmin do\n" do
       <<-END
-    resources :#{file_name}
+    resources :#{plural_name}
       END
     end
   end
 
   def create_controller
-    template "controller.rb", File.join("app/controllers", class_path, "#{file_name}_controller.rb")
+    template "resource.rb", File.join("app/controllers", class_path, "#{plural_file_name}_controller.rb")
   end
 end
--- lib/generators/godmin/resource/templates/resource.rb
+++ lib/generators/godmin/resource/templates/resource.rb
@@ -3,7 +3,7 @@ require_dependency "<%= namespaced_path %>/application_controller"
 
 <% end -%>
 <% module_namespacing do -%>
-class <%= class_name %>Controller < ApplicationController
+class <%= class_name.pluralize %>Controller < ApplicationController
   include Godmin::Resource
 
   def attrs_for_index
