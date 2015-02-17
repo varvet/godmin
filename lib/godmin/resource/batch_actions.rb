c01b839a460ef837f750e03a76ea4c037644f7f1
Jens Ljungblad <jens.ljungblad@gmail.com>
2015-02-17 10:58:12 +0100

    Use guard clause

--- lib/godmin/resource/batch_actions.rb
+++ lib/godmin/resource/batch_actions.rb
@@ -11,39 +11,21 @@ module Godmin
         self.class.batch_action_map
       end
 
-      # def batch_action
-      #   action   = params[:batch_action][:action]
-      #   item_ids = params[:batch_action][:items].keys.map(&:to_i)
-      #
-      #   if batch_action_map.key?(action.to_sym)
-      #     # Store the batched item ids so they can be highlighted later
-      #     flash[:batch_actioned_ids] = item_ids
-      #
-      #     # If the batch action returns false, it is because it has implemented
-      #     # its own redirect. Therefore we return wihout redirecting.
-      #     return unless send("batch_action_#{action}", resource_class.find(item_ids))
-      #   end
-      #
-      #   redirect_to :back
-      # end
-
       def update
-        if params[:id].include?(",")
-          item_ids = params[:id].split(",").map(&:to_i)
+        return super unless params[:id].include?(",")
 
-          if batch_action_map.key?(params[:batch_action][:action].to_sym)
-            # Store the batched item ids so they can be highlighted later
-            flash[:batch_actioned_ids] = item_ids
+        item_ids = params[:id].split(",").map(&:to_i)
 
-            # If the batch action returns false, it is because it has implemented
-            # its own redirect. Therefore we return wihout redirecting.
-            return unless send("batch_action_#{params[:batch_action][:action]}", resource_class.find(item_ids))
-          end
+        if batch_action_map.key?(params[:batch_action][:action].to_sym)
+          # Store the batched item ids so they can be highlighted later
+          flash[:batch_actioned_ids] = item_ids
 
-          redirect_to :back
-        else
-          super
+          # If the batch action returns false, it is because it has implemented
+          # its own redirect. Therefore we return wihout redirecting.
+          return unless send("batch_action_#{params[:batch_action][:action]}", resource_class.find(item_ids))
         end
+
+        redirect_to :back
       end
 
       module ClassMethods
