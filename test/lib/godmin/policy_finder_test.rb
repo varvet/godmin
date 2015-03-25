b723fa6cb9559f553a1669a3f408fe4b5f1eeca3
Linus Pettersson <linus.pettersson@gmail.com>
2015-03-25 10:57:51 +0100

    Update policy tests and readme

--- test/lib/godmin/policy_finder_test.rb
+++ test/lib/godmin/policy_finder_test.rb
@@ -1,10 +1,12 @@
 require "test_helper"
 
-module Namespace; end
-class Namespace::ArticlePolicy; end
-class Namespace::ObjectPolicy; end
-class Article; extend ActiveModel::Naming; end
-class Overridden
+module Namespace
+  class ArticlePolicyTestPolicy; end
+  class ObjectPolicy; end
+end
+
+class ArticlePolicyTest; extend ActiveModel::Naming; end
+class OverriddenPolicyTest
   extend ActiveModel::Naming
   def self.policy_class
     Namespace::ObjectPolicy
@@ -20,8 +22,8 @@ module Godmin
     class PolicyFinderTest < ActiveSupport::TestCase
       def test_find_by_model
         namespaced_as "namespace" do
-          policy = PolicyFinder.find(Article)
-          assert_equal Namespace::ArticlePolicy, policy
+          policy = PolicyFinder.find(ArticlePolicyTest)
+          assert_equal Namespace::ArticlePolicyTestPolicy, policy
         end
       end
 
@@ -34,21 +36,21 @@ module Godmin
 
       def test_find_by_symbol
         namespaced_as "namespace" do
-          policy = PolicyFinder.find(:article)
-          assert_equal Namespace::ArticlePolicy, policy
+          policy = PolicyFinder.find(:article_policy_test)
+          assert_equal Namespace::ArticlePolicyTestPolicy, policy
         end
       end
 
       def test_override_policy_class_on_class
         namespaced_as "namespace" do
-          policy = PolicyFinder.find(Overridden)
+          policy = PolicyFinder.find(OverriddenPolicyTest)
           assert_equal Namespace::ObjectPolicy, policy
         end
       end
 
       def test_override_policy_class_on_instance
         namespaced_as "namespace" do
-          policy = PolicyFinder.find(Overridden.new)
+          policy = PolicyFinder.find(OverriddenPolicyTest.new)
           assert_equal Namespace::ObjectPolicy, policy
         end
       end
