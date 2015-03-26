c384d2751c127aba9587025b51cde500202e3fd8
Jens Ljungblad <jens.ljungblad@gmail.com>
2015-03-26 11:37:54 +0100

    Fixes filter alignment + use bootstrap form for filters

--- lib/godmin/helpers/filters.rb
+++ lib/godmin/helpers/filters.rb
@@ -1,110 +1,108 @@
 module Godmin
   module Helpers
     module Filters
-      def filter_input_tag(name, options, html_options = {})
+      def filter_form
+        bootstrap_form_tag url: params, method: :get, layout: :inline, builder: FormBuilders::FilterFormBuilder do |f|
+          yield(f)
+        end
+      end
+    end
+  end
+
+  module FormBuilders
+    class FilterFormBuilder < BootstrapForm::FormBuilder
+      def filter_field(name, options, html_options = {})
         case options[:as]
         when :string
-          filter_string_tag(name, options, html_options)
+          string_filter_field(name, options, html_options)
         when :select
-          filter_select_tag(name, options)
+          select_filter_field(name, options, html_options)
         when :multiselect
-          filter_multiselect_tag(name, options)
-        when :checkboxes
-          filter_checkbox_tags(name, options)
+          multiselect_filter_field(name, options, html_options)
         end
       end
 
-      private
-
-      def filter_string_tag(name, _options, html_options)
-        text_field_tag(
-          name,
-          default_filter_value(name),
-          { name: "filter[#{name}]",
-            class: "form-control",
-            placeholder: translate_scoped("filters.labels.#{name}", default: name.to_s.titleize)
+      def string_filter_field(name, _options, html_options = {})
+        text_field(
+          name, {
+            name: "filter[#{name}]",
+            value: default_filter_value(name),
+            placeholder: @template.translate_scoped("filters.labels.#{name}", default: name.to_s.titleize),
+            wrapper_class: "filter"
           }.deep_merge(html_options)
         )
       end
 
-      def filter_select_tag(name, options)
-        filter_select_tag_helper(
-          name,
-          options,
-          name: "filter[#{name}]",
-          include_blank: true,
-          class: "form-control",
-          data: {
-            behavior: "select-box",
-            placeholder: translate_scoped("filters.select.placeholder.one")
-          }
+      def select_filter_field(name, options, html_options = {})
+        filter_select(
+          name, options, {
+            name: "filter[#{name}]",
+            data: {
+              placeholder: @template.translate_scoped("filters.select.placeholder.one")
+            }
+          }.deep_merge(html_options)
         )
       end
 
-      def filter_multiselect_tag(name, options)
-        filter_select_tag_helper(
-          name,
-          options,
-          name: "filter[#{name}][]",
-          multiple: true,
-          class: "form-control",
-          data: {
-            behavior: "select-box",
-            placeholder: translate_scoped("filters.select.placeholder.many")
-          }
+      def multiselect_filter_field(name, options, html_options = {})
+        filter_select(
+          name, options, {
+            name: "filter[#{name}][]",
+            multiple: true,
+            data: {
+              placeholder: @template.translate_scoped("filters.select.placeholder.many")
+            }
+          }.deep_merge(html_options)
         )
       end
 
-      def filter_select_tag_helper(name, options, html_options)
+      def apply_filters_button
+        submit @template.translate_scoped("filters.buttons.apply")
+      end
+
+      def clear_filters_button
+        @template.link_to(
+          @template.translate_scoped("filters.buttons.clear"),
+          @template.url_for(
+            @template.params.slice(:scope, :order)
+          ),
+          class: "btn btn-default"
+        )
+      end
+
+      private
+
+      def filter_select(name, options, html_options)
         unless options[:collection].is_a? Proc
-          raise "A collection proc must be specified for select filters"
+          fail "A collection proc must be specified for select filters"
         end
 
         collection = options[:collection].call
 
-        if collection.is_a? ActiveRecord::Relation
-          choices = options_from_collection_for_select(
-            collection,
-            options[:option_value],
-            options[:option_text],
-            selected: default_filter_value(name)
-          )
-        else
-          choices = options_for_select(
-            collection,
-            selected: default_filter_value(name)
-          )
-        end
-
-        select_tag(name, choices, html_options)
-      end
-
-      def filter_checkbox_tags(name, options)
-        unless options[:collection].is_a? Proc
-          raise "A collection proc must be specified for checkbox filters"
-        end
-
-        collection = options[:collection].call
-
-        collection.map do |item|
-          text, value = if !item.is_a?(String) && item.respond_to?(:first) && item.respond_to?(:last)
-            [item.first.to_s, item.last.to_s]
+        choices =
+          if collection.is_a? ActiveRecord::Relation
+            @template.options_from_collection_for_select(
+              collection,
+              options[:option_value],
+              options[:option_text],
+              selected: default_filter_value(name)
+            )
           else
-            [item.to_s, item.to_s]
+            @template.options_for_select(
+              collection,
+              selected: default_filter_value(name)
+            )
           end
 
-          is_checked = default_filter_value(name) ? default_filter_value(name).include?(value) : false
-
-          content_tag :div, class: "checkbox" do
-            label_tag("#{name}_#{value}") do
-              check_box_tag("filter[#{name}][]", value, is_checked, id: "#{name}_#{value}") << text
-            end
-          end
-        end.join("\n").html_safe
+        select(
+          name, choices, { include_blank: true, wrapper_class: "filter" }, {
+            data: { behavior: "select-box" }
+          }.deep_merge(html_options)
+        )
       end
 
       def default_filter_value(name)
-        params[:filter] ? params[:filter][name] : nil
+        @template.params[:filter] ? @template.params[:filter][name] : nil
       end
     end
   end
