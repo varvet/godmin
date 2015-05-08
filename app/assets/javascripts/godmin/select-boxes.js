6356d2967f94f041e347edbd414f9ef6d4a90bb0
Linus Pettersson <linus.pettersson@gmail.com>
2015-05-08 10:35:32 +0200

    Allow users to specify a custom add text as data attr

--- app/assets/javascripts/godmin/select-boxes.js
+++ app/assets/javascripts/godmin/select-boxes.js
@@ -14,7 +14,12 @@ Godmin.SelectBoxes = (function() {
 
   function initializeSelectBox($el, options) {
     var defaults = {
-      inputClass: 'selectize-input'
+      inputClass: 'selectize-input',
+      render: {
+        option_create: function(data, escape) {
+          return '<div class="create">' + (this.$input.data("add-text") || "Add") + ' <strong>' + escape(data.input) + '</strong>&hellip;</div>';
+        }
+      }
     };
 
     $el.selectize($.extend(defaults, options));
