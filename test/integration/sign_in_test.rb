3d4c2dd65e4c4225b92d2b5ba7879bb8a37bf896
Jens Ljungblad <jens.ljungblad@gmail.com>
2015-09-07 15:48:53 +0200

    Test sign out

--- test/integration/sign_in_test.rb
+++ test/integration/sign_in_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class SignInTest < ActionDispatch::IntegrationTest
-  def test_sign_in
+  def test_sign_in_and_out
     AdminUser.create!(email: "admin@example.com", password: "password")
     visit secret_articles_path
     assert_not_equal secret_articles_path, current_path
@@ -10,5 +10,8 @@ class SignInTest < ActionDispatch::IntegrationTest
     click_button "Sign in"
     visit secret_articles_path
     assert_equal secret_articles_path, current_path
+    click_link "Sign out"
+    visit secret_articles_path
+    assert_not_equal secret_articles_path, current_path
   end
 end
