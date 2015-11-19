d13a127d6fdc5a0d12b9608070abae5ef99fdc1a
Jon Kindell <jon.kindell@varvet.com>
2015-11-19 17:22:03 +0100

    Bump version

--- template.rb
+++ template.rb
@@ -29,7 +29,7 @@ def install_engine
 
   inject_into_file "admin/admin.gemspec", before: /^end/ do
     <<-END.strip_heredoc.indent(2)
-      s.add_dependency "godmin", "> 0.12"
+      s.add_dependency "godmin", "~> 1.0.0"
     END
   end
 
