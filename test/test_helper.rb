7a19a9940048451574a953ec72ebb52cbe6df4bd
Jens Ljungblad <jens.ljungblad@gmail.com>
2015-11-26 16:40:05 +0100

    Fixes transaction issue with capybara js tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -29,6 +29,19 @@ if ActiveSupport::TestCase.method_defined?(:fixture_path=)
   ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
 end
 
+class ActiveRecord::Base
+  mattr_accessor :shared_connection
+  @@shared_connection = nil
+
+  def self.connection
+    @@shared_connection || retrieve_connection
+  end
+end
+
+# Forces all threads to share the same connection. This works on
+# Capybara because it starts the web server in a thread.
+ActiveRecord::Base.shared_connection = ActiveRecord::Base.connection
+
 class ActionDispatch::IntegrationTest
   include Capybara::DSL
 
