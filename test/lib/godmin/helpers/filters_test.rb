cec06a90c2fb63e39ef21d61b13d39591f86c022
Linus Pettersson <linus.pettersson@gmail.com>
2016-04-20 23:41:00 +0200

    Add some filter helper tests

--- test/lib/godmin/helpers/filters_test.rb
+++ test/lib/godmin/helpers/filters_test.rb
@@ -13,13 +13,36 @@ module Godmin
         end
       end
 
-      # TODO: actually implement this test properly, apparently
-      # assert_select can be used if the helper output is parsed
-      # with HTML::Document first...
-      def test_string_filter_field
-        form = filter_form url: "/" do |f|
+      def test_string_renders_as_string
+        @output_buffer = filter_form url: "/" do |f|
           f.filter_field :foo, as: :string
         end
+
+        assert_select "input[type=text][name=?]", "filter[foo]", 1, "No text input found with name foo"
+      end
+
+      def test_string_filter_field_renders_text_field
+        @output_buffer = filter_form url: "/" do |f|
+          f.string_filter_field :foo, {}, value: "Foobar"
+        end
+
+        assert_select "label[for=foo]", 1, "No label was found"
+        assert_select "input[type=text][name=?]", "filter[foo]", 1, "No text input found with name foo" do |element|
+          assert_equal "Foo", element.attr("placeholder").text
+          assert_equal "Foobar", element.attr("value").text
+        end
+      end
+
+      def test_multiselect_filter_field_includes_no_hidden
+        @output_buffer = filter_form url: "/" do |f|
+          f.multiselect_filter_field :foo, collection: -> { %w(Foo Bar) }
+        end
+
+        assert_select "label[for=foo]", 1, "No label was found"
+        assert_select "select[name=?]", "filter[foo][]", 1, "No text input found with name foo" do
+          assert_select "option", 3
+        end
+        assert_select "input[type=hidden][name=?]", "filter[foo][]", 0
       end
     end
   end
