f9dc8456bedf34cee982a1d25a9ce8feafe16f90
Jens Ljungblad <jens.ljungblad@gmail.com>
2016-07-08 10:44:58 +0200

    Refactors filter helper tests

--- test/lib/godmin/helpers/filters_test.rb
+++ test/lib/godmin/helpers/filters_test.rb
@@ -13,17 +13,9 @@ module Godmin
         end
       end
 
-      def test_string_renders_as_string
+      def test_filter_field_as_string
         @output_buffer = filter_form url: "/" do |f|
-          f.filter_field :foo, as: :string
-        end
-
-        assert_select "input[type=text][name=?]", "filter[foo]", 1, "No text input found with name foo"
-      end
-
-      def test_string_filter_field_renders_text_field
-        @output_buffer = filter_form url: "/" do |f|
-          f.string_filter_field :foo, {}, value: "Foobar"
+          f.filter_field :foo, { as: :string }, value: "Foobar"
         end
 
         assert_select "label[for=foo]", 1, "No label was found"
@@ -33,15 +25,23 @@ module Godmin
         end
       end
 
-      def test_multiselect_filter_field_includes_no_hidden
+      def test_filter_field_as_multiselect
         @output_buffer = filter_form url: "/" do |f|
-          f.multiselect_filter_field :foo, collection: -> { %w(Foo Bar) }
+          f.filter_field :foo, as: :multiselect, collection: -> { %w(Foo Bar) }
         end
 
         assert_select "label[for=foo]", 1, "No label was found"
-        assert_select "select[name=?]", "filter[foo][]", 1, "No text input found with name foo" do
+        assert_select "select[name=?]", "filter[foo][]", 1, "No select found with name foo" do
           assert_select "option", 3
         end
+        assert_select "input[type=hidden][name=?]", "filter[foo][]", 0
+      end
+
+      def test_filter_field_as_multiselect_includes_no_hidden_field
+        @output_buffer = filter_form url: "/" do |f|
+          f.filter_field :foo, as: :multiselect, collection: -> { %w(Foo Bar) }
+        end
+
         assert_select "input[type=hidden][name=?]", "filter[foo][]", 0
       end
     end
