572d269757e78984629851526726957cd67d3729
Jens Ljungblad <jens.ljungblad@gmail.com>
2017-02-16 11:17:42 +0100

    Schema

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -40,8 +39,7 @@ ActiveRecord::Schema.define(version: 20170207081043) do
     t.integer "article_id"
     t.string  "title"
     t.text    "body"
+    t.index ["article_id"], name: "index_comments_on_article_id"
   end
-
-  add_index "comments", ["article_id"], name: "index_comments_on_article_id"
 
 end
